e9e5e1abe2cebaaa1ee3d6564077d6e3417590a7
EvgenRud <rudnik.evgen@gmail.com>
2023-03-07 15:42:15 +0400

    Построение списка с методом split и конкатенации

---
+++ lvl_1/answer_1.1.py
@@ -0,0 +1,11 @@
+# Задача 1.1.
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+result = my_favorite_songs.split(',')
+
+print(result[0] + result[-1] + result[1] + result[-2])
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
